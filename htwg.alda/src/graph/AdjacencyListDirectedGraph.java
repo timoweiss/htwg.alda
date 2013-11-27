118a2a255753e53bea47e0516086d2bf0e5e916a
miknoch <michael-knoch@gmx.net>
2013-11-27 14:29:29 +0100

    added 2 Classes

---
+++ htwg.alda/src/graph/AdjacencyListDirectedGraph.java
@@ -0,0 +1,117 @@
+package graph;
+
+import java.util.List;
+
+
+
+public class AdjacencyListDirectedGraph implements DirectedGraph {
+
+	@Override
+	public boolean addVertex(Object v) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean addEdge(Object v, Object w) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean addEdge(Object v, Object w, double weight) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean containsVertex(Object v) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean containsEdge(Object v, Object w) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public double getWeight(Object v, Object w) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int getNumberOfVertexes() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int getNumberOfEdges() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public List getVertexList() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List getEdgeList() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List getAdjacentVertexList(Object v) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List getIncidentEdgeList(Object v) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public int getInDegree(Object v) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int getOutDegree(Object v) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public List getPredecessorVertexList(Object v) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List getSuccessorVertexList(Object v) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List getOutgoingEdgeList(Object v) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List getIncomingEdgeList(Object v) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
---
+++ htwg.alda/src/graph/AdjacencyListUndirectedGraph.java
@@ -0,0 +1,85 @@
+package graph;
+
+import java.util.List;
+
+public class AdjacencyListUndirectedGraph implements UndirectedGraph {
+
+	@Override
+	public boolean addVertex(Object v) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean addEdge(Object v, Object w) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean addEdge(Object v, Object w, double weight) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean containsVertex(Object v) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean containsEdge(Object v, Object w) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public double getWeight(Object v, Object w) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int getNumberOfVertexes() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int getNumberOfEdges() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public List getVertexList() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List getEdgeList() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List getAdjacentVertexList(Object v) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List getIncidentEdgeList(Object v) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public int getDegree(Object v) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}

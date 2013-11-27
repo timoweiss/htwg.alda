33af49a442c5ca4dcce955b6e377a81bc163dd5b
timoweiss <timoweiss@141.37.205.146>
2013-11-27 18:50:01 +0100

    added breadthFirstSearch()

--- htwg.alda/src/graph/GraphTraversion.java
+++ htwg.alda/src/graph/GraphTraversion.java
@@ -2,9 +2,12 @@ package graph;
 
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Queue;
 
 public class GraphTraversion {
 	
+	
+	// #1
 	public static <V> List<V> depthFirstSearch(Graph<V> g, V s) {
 		List<V> visited = new LinkedList<V>();
 		depthFirstSearchR(g, s, visited);
@@ -19,7 +22,35 @@ public class GraphTraversion {
 				depthFirstSearchR(g, x, v);
 			}
 		}
+		
 	}
+	
+	// #2
+	public static <V> List<V> breadthFirstSearch(Graph<V> g, V s) {
+		List<V> visited = new LinkedList<V>();
+		Queue<V> queue = new LinkedList<V>();
+		queue.add(s);
+		
+		while(!queue.isEmpty()) {
+			s = queue.remove();
+			if(visited.contains(s)) {
+				continue;
+			}
+			visited.add(s);
+			for(V x : g.getAdjacentVertexList(s)) {
+				if(!visited.contains(x)) {
+					queue.add(x);
+				}
+			}
+		}
+		
+		return visited;
+		
+	}
+	
+	// #3
+	
+	
 	
 
 }

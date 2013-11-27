29cb1399eb26bc5b4dbec5be188f2d9cfb011562
timoweiss <timoweiss@141.37.205.146>
2013-11-27 20:27:12 +0100

    extended testing

--- htwg.alda/src/graph/GraphTraversion.java
+++ htwg.alda/src/graph/GraphTraversion.java
@@ -6,78 +6,82 @@ import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
 import java.util.Queue;
+import java.util.Set;
+import java.util.TreeSet;
 
 public class GraphTraversion {
-	
-	
+
 	// #1
 	public static <V> List<V> depthFirstSearch(Graph<V> g, V s) {
-		List<V> visited = new LinkedList<V>();
-		depthFirstSearchR(g, s, visited);
-		return visited;
+		List<V> searchList = new LinkedList<V>();
+		Set<V> visited = new TreeSet<V>();
+
+		visitDeepR(g, s, visited, searchList);
+
+		return searchList;
 	}
-	
-	private static <V> void depthFirstSearchR(Graph<V> g, V s, List<V> v) {
-		v.add(s);
-		for(V x : g.getAdjacentVertexList(s)) {
-			if(!g.containsVertex(x)) {
-				depthFirstSearchR(g, x, v);
+
+	private static <V> void visitDeepR(Graph<V> g, V v, Set<V> visited,
+			List<V> list) {
+		visited.add(v);
+		list.add(v);
+
+		for (V vertex : g.getAdjacentVertexList(v)) {
+			if (!visited.contains(vertex)) {
+				visitDeepR(g, vertex, visited, list);
 			}
 		}
-		
 	}
-	
+
 	// #2
 	public static <V> List<V> breadthFirstSearch(Graph<V> g, V s) {
 		List<V> visited = new LinkedList<V>();
 		Queue<V> queue = new LinkedList<V>();
 		queue.add(s);
-		
-		while(!queue.isEmpty()) {
+
+		while (!queue.isEmpty()) {
 			s = queue.remove();
-			if(visited.contains(s)) {
+			if (visited.contains(s)) {
 				continue;
 			}
 			visited.add(s);
-			for(V x : g.getAdjacentVertexList(s)) {
-				if(!visited.contains(x)) {
+			for (V x : g.getAdjacentVertexList(s)) {
+				if (!visited.contains(x)) {
 					queue.add(x);
 				}
 			}
 		}
-		
+
 		return visited;
-		
+
 	}
-	
+
 	// #3
 	public static <V> List<V> topologicalSort(DirectedGraph<V> g) {
-        List<V> topologicalSortedList = new LinkedList<>();
-        Map<V, Integer> inDegree = new HashMap<>();
-        Queue<V> q = new ArrayDeque<>();
-        for (V v : g.getVertexList()) {
-                inDegree.put(v, g.getInDegree(v));
-                if (inDegree.get(v) == 0) {
-                        q.add(v);
-                }
-        }
-        while (!q.isEmpty()) {
-                V v = q.remove();
-                topologicalSortedList.add(v);
-                for (V w : g.getSuccessorVertexList(v)) {
-                        inDegree.put(w, inDegree.get(w) - 1);
+		List<V> topologicalSortedList = new LinkedList<>();
+		Map<V, Integer> inDegree = new HashMap<>();
+		Queue<V> q = new ArrayDeque<>();
+		for (V v : g.getVertexList()) {
+			inDegree.put(v, g.getInDegree(v));
+			if (inDegree.get(v) == 0) {
+				q.add(v);
+			}
+		}
+		while (!q.isEmpty()) {
+			V v = q.remove();
+			topologicalSortedList.add(v);
+			for (V w : g.getSuccessorVertexList(v)) {
+				inDegree.put(w, inDegree.get(w) - 1);
 
-                        if (inDegree.get(w) == 0) {
-                                q.add(w);
-                        }
-                }
-        }
-        if (topologicalSortedList.size() != g.getNumberOfVertexes()) {
-                return null;
-        }
-        return topologicalSortedList;
-}
-	
-	
+				if (inDegree.get(w) == 0) {
+					q.add(w);
+				}
+			}
+		}
+		if (topologicalSortedList.size() != g.getNumberOfVertexes()) {
+			return null;
+		}
+		return topologicalSortedList;
+	}
 
 }
--- htwg.alda/src/graph/main.java
+++ htwg.alda/src/graph/main.java
@@ -14,10 +14,16 @@ public class main {
 			graph1.addVertex(i);
 			graph2.addVertex(i);
 		}
-		for(int i = 0; i <= 100; i++) {
-			if(i != i%5 && i%5 != i%60) {
-				graph1.addEdge(i%60, i%5, 1.0);
-				graph2.addEdge(i%60, i%5, 1.0);
+		for(int i = 0; i <= 78; i++) {
+			int j = (int) (Math.random() * 78);
+			if(j != i && j%77 != i && j%33 != i) {
+				
+				graph1.addEdge(i, j%33);
+				
+				graph2.addEdge(i, j%77);
+				graph2.addEdge(i, j%33);
+				graph2.addEdge(i, j);
+				
 			}	
 		}
 
@@ -32,6 +38,50 @@ public class main {
 
 		System.out.println("depthFirstSearch: \t" + GraphTraversion.depthFirstSearch(graph2, 2));
 		System.out.println();
+		
+		
+		
+		
+		//Steffano
+		AdjacencyListDirectedGraph<Integer> dirGraph = new AdjacencyListDirectedGraph<Integer>();
+        AdjacencyListUndirectedGraph<Integer> undirGraph = new AdjacencyListUndirectedGraph<Integer>();
+
+        for (int i = 0; i < 15; i++) {
+                dirGraph.addVertex(i);
+                undirGraph.addVertex(i);
+        }
+        
+        for (int i = 0; i< 15; i++) {
+                int j = (int) (Math.random() * 15);
+                if (i != j || i!= j%5 || i!= j%3) {
+                        dirGraph.addEdge(i, j);
+                        dirGraph.addEdge(i, j%5);
+                        dirGraph.addEdge(i, j%3);
+                }
+        }
+        
+        for (int i = 0; i< 10;i++) {
+                int j = (int) (Math.random() * 10);
+                if (i != j) {
+                        undirGraph.addEdge(i, j);
+                }
+        }
+        
+        System.out.println("Breadth Search:");
+        System.out.println("Direct Graph:");
+        System.out.println(GraphTraversion.breadthFirstSearch(dirGraph, 0));
+        System.out.println("Undirect Graph:");
+        System.out.println(GraphTraversion.breadthFirstSearch(undirGraph, 0));
+        
+        System.out.println("Depth Search:");
+        System.out.println("Direct Graph:");
+        System.out.println(GraphTraversion.depthFirstSearch(dirGraph, 0));
+        System.out.println("Undirect Graph:");
+        System.out.println(GraphTraversion.depthFirstSearch(undirGraph, 0));
+        
+        System.out.println("Topological Sort:");
+        System.out.println(GraphTraversion.topologicalSort(dirGraph));
+
 		
 	}
 

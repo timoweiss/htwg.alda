cc8761da8ac04c6c387dc8cbf34dce4136d3ef51
Michael Knoch <michael-knoch@gmx.net>
2013-11-28 10:42:26 +0100

    removes some warnings

--- htwg.alda/src/aufgabe2/graph/main.java
+++ htwg.alda/src/aufgabe2/graph/main.java
@@ -5,7 +5,7 @@ public class main {
 	/**
 	 * @param args
 	 */
-	public static void main(String[] args) {
+	public static void main(final String[] args) {
 		
 		UndirectedGraph<Integer> graph1 = new AdjacencyListUndirectedGraph<>();
 		DirectedGraph<Integer> graph2 = new AdjacencyListDirectedGraph<>();
